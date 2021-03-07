4ccfd3c618c8c20df7a761a3dde2c589b0a0a71d
Alisha Budhathoki <alishabudhathoki69@gmail.com>
2021-03-07 07:51:24 +0545

    adding a container with color as canvas for painting

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,24 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+final Color darkBlue = Color.fromARGB(255, 18, 32, 47);
 void main() {
   runApp(MyApp());
 }
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: darkBlue),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(
+          color: Colors.white,
+          padding: EdgeInsets .symmetric(horizontal: 40, vertical: 80),
+          child: Container(
+            color: Colors.yellow,
+          ),
+        ),
+      ),
+    );
   }
 }
