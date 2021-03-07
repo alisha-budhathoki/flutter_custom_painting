13f1e4ed7903ec9508ed9ae2eb194b4853c50fab
Alisha Budhathoki <alishabudhathoki69@gmail.com>
2021-03-07 19:31:22 +0545

    smiley face in flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
 void main() {
   runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -12,14 +14,15 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         body: Container(
           color: Colors.white,
-          padding: EdgeInsets .symmetric(horizontal: 40, vertical: 80),
+          padding: EdgeInsets.symmetric(horizontal: 40, vertical: 80),
           child: LayoutBuilder(
-            builder: (_, constraints) =>Container(
+            builder: (_, constraints) => Container(
               width: constraints.widthConstraints().maxWidth,
               height: constraints.heightConstraints().maxHeight,
               color: Colors.yellow,
-              child:
-              CustomPaint(painter  : FaceOutlinePainter(),),
+              child: CustomPaint(
+                painter: FaceOutlinePainter(),
+              ),
             ),
           ),
         ),
@@ -27,26 +30,36 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-class FaceOutlinePainter extends CustomPainter{
+
+class FaceOutlinePainter extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
     //Defining a paint object
     final paint = Paint()
-    ..style = PaintingStyle.stroke
-    ..strokeWidth = 4.0
-    ..color = Colors.indigo;
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 4.0
+      ..color = Colors.indigo;
     // canvas.drawRect(
     //   Rect.fromLTWH(20, 40, 100, 100),
     //   paint
     // );
-    canvas.drawRRect(RRect.fromRectAndRadius(Rect.fromLTWH(20, 40, 100, 100),
-    Radius.circular(20)),
-    paint);
+    canvas.drawRRect(
+        RRect.fromRectAndRadius(
+            Rect.fromLTWH(20, 40, 100, 100), Radius.circular(20)),
+        paint);
 
-    canvas.drawOval(Rect.fromLTWH(size.width-120, 40, 100, 100), paint);
+    canvas.drawOval(Rect.fromLTWH(size.width - 120, 40, 100, 100), paint);
+
+    final mouth = Path();
+    mouth.moveTo(size.width * 0.8, size.height * 0.6);
+    mouth.arcToPoint(Offset(size.width * 0.2, size.height * 0.6),
+        radius: Radius.circular(150), clockwise: true);
+    mouth.arcToPoint(Offset(size.width * 0.8, size.height * 0.6),
+        radius: Radius.circular(250), clockwise: true);
+
+    canvas.drawPath(mouth, paint);
   }
 
   @override
   bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
-
 }
